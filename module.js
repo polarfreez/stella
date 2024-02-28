ad7ddb6f8fa41526dc770d88668858cb83c62ed5
StoltZ <83966759+stoltz-dev@users.noreply.github.com>
2024-02-28 02:30:14 -0300

    Update module.js

--- module.js
+++ module.js
@@ -499,7 +499,7 @@ async function createImage(prompt) {
   const app = await client("multimodalart/stable-cascade");
   const result = await app.predict("/run", [		
   				prompt, // string  in 'Prompt' Textbox component		
-  				"", // string  in 'Negative prompt' Textbox component		
+  				"verybadimagenegative_v1.3, ng_deepnegative_v1_75t, (ugly face:0.8),cross-eyed,sketches, (worst quality:2), (low quality:2), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, bad anatomy, DeepNegative, facing away, tilted head, {Multiple people}, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worstquality, low quality, normal quality, jpegartifacts, signature, watermark, username, blurry, bad feet, cropped, poorly drawn hands, poorly drawn face, mutation, deformed, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, extra fingers, fewer digits, extra limbs, extra arms,extra legs, malformed limbs, fused fingers, too many fingers, long neck, cross-eyed,mutated hands, polar lowres, bad body, bad proportions, gross proportions, text, error, missing fingers, missing arms, missing legs, extra digit, extra arms, extra leg, extra foot, ((repeating hair))", // string  in 'Negative prompt' Textbox component		
   				0, // number (numeric value between 0 and 2147483647) in 'Seed' Slider component		
   				1024, // number (numeric value between 1024 and 1536) in 'Width' Slider component		
   				1024, // number (numeric value between 1024 and 1536) in 'Height' Slider component		
